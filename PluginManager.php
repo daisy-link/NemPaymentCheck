53ba532ac34f733adfc7f7af8a057c3ffefff3ac
izayoi256 <qwert.izayoi@gmail.com>
2018-04-24 19:47:49 +0900

    ライセンス表記を追加

--- PluginManager.php
+++ PluginManager.php
@@ -1,6 +1,9 @@
 <?php
 /*
  * Copyright(c) 2018 Daisy Inc. All Rights Reserved.
+ *
+ * This software is released under the MIT license.
+ * http://opensource.org/licenses/mit-license.php
  */
 
 namespace Plugin\NemPaymentCheck;
